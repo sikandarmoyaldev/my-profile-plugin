4bc856e3dcaf31616881feaadbeeea18f50e0fa8
sikandarmoyaldev <165464126+sikandarmoyaldev@users.noreply.github.com>
2026-05-06 04:55:59 +0000

    Fix styling

--- src/MyProfilePluginServiceProvider.php
+++ src/MyProfilePluginServiceProvider.php
@@ -10,11 +10,11 @@ use Filament\Support\Facades\FilamentAsset;
 use Filament\Support\Facades\FilamentIcon;
 use Illuminate\Filesystem\Filesystem;
 use Livewire\Features\SupportTesting\Testable;
+use Sikandarmoyaldev\MyProfilePlugin\Commands\MyProfilePluginCommand;
+use Sikandarmoyaldev\MyProfilePlugin\Testing\TestsMyProfilePlugin;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Sikandarmoyaldev\MyProfilePlugin\Commands\MyProfilePluginCommand;
-use Sikandarmoyaldev\MyProfilePlugin\Testing\TestsMyProfilePlugin;
 
 class MyProfilePluginServiceProvider extends PackageServiceProvider
 {
